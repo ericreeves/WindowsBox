87ae40881d8598c33f3da9ab1bc6987b7f36a7ed
Eric Reeves <eric@alluvium.com>
2022-09-22 11:37:00 -0500

    Updates

--- ahk/scripts/Start-Yasb.ps1
+++ ahk/scripts/Start-Yasb.ps1
@@ -1,5 +1,15 @@
 . "$PSScriptRoot\Common.ps1"
 $ErrorActionPreference = 'SilentlyContinue'
+
+Write-Output-Format "[yasb] Copying Configs to .yasb"
+New-Item -ItemType Directory -Force -Path $Yasb_Config_Folder
+$ConfigFiles = Get-ChildItem -Path "$WindowsBox_Folder\yasb-config\" -File
+foreach ($ConfigFile in $ConfigFiles)
+{
+    Write-Output-Format "[yasb] Copying $ConfigFile to .yasb"
+	Copy-Item "$WindowsBox_Folder\yasb-config\$ConfigFile" "$Yasb_Config_Folder" -force
+}
+
 Write-Output-Format "[yasb] Starting yasb"
 Start-Process -FilePath "$Python_Bin_Folder\pythonw.exe" -ArgumentList "src/main.py" -WorkingDirectory "$Yasb_Folder" -WindowStyle Hidden
 
