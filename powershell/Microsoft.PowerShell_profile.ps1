51c2f80fdf474e389f7599364836692ba55ef5d0
ericreeves <eric@alluvium.com>
2023-04-22 12:27:04 -0500

    Add PS Profile

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,15 +1,28 @@
 function refresh-path {
     $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") +
                 ";" +
-                [System.Environment]::GetEnvironmentVariable("Path","User")
+               [System.Environment]::GetEnvironmentVariable("Path","User")
 }
 
 #oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\paradox.omp.json" | Invoke-Expression
 #paradox
 # M365Princess
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\night-owl.omp.json" | Invoke-Expression
+#oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\night-owl.omp.json" | Invoke-Expression
+
+oh-my-posh --init --shell pwsh --config "$env:POSH_THEMES_PATH\M365Princess.omp.json" | Invoke-Expression
+Import-Module posh-git
+Import-Module -Name Terminal-Icons
+# Set-PoshPrompt -Theme bubbles
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+# Autocompletion for arrow keys
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+# auto suggestions
+Import-Module PSReadLine
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -EditMode Windows
+Set-PSReadLineOption -PredictionViewStyle ListView
 
 #Invoke-Expression (&starship init powershell)
 
-Import-Module -Name Terminal-Icons
-
+$env:Path = [System.Environment]::ExpandEnvironmentVariables([System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User"))
