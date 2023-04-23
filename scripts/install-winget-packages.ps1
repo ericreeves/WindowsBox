503c34bbb45107f1a06de61c735fd71fa792ca4c
ericreeves <eric@alluvium.com>
2023-04-23 13:01:56 -0500

    asdf

--- scripts/install-winget-packages.ps1
+++ scripts/install-winget-packages.ps1
@@ -2,65 +2,56 @@
 # WinGet Packages to Install
 #
 $apps = @(
-    @{ name = "1Password" },
-    # @{ name = "7zip.7zip" },
+    @{ name = "1Password" }
+    # @{ name = "7zip.7zip" }
     @{ name = "9P1741LKHQS9" }
-    @{ name = "AgileBits.1Password.CLI" },
-    @{ name = "Audacity.Audacity" },
-    @{ name = "Discord.Discord" },
-    @{ name = "Ferdium.Ferdium" },
-#    @{ name = "Flameshot.Flameshot" },
-    @{ name = "gerardog.gsudo" },
-    @{ name = "Git.Git"; interactive = "true" },
-    @{ name = "GitHub.GitHubDesktop" },
-    @{ name = "GnuPG.Gpg4win"; interactive = "true" },
-    @{ name = "Google.Chrome"; force = "true" },
-    @{ name = "gsass1.NTop" },
-    @{ name = "Hashicorp.Packer" },
-    @{ name = "Hashicorp.Terraform" },
-    @{ name = "JackieLiu.NotepadsApp" },
-    @{ name = "JanDeDobbeleer.OhMyPosh" },
-    @{ name = "Keybase" },
-    @{ name = "Lexikos.AutoHotkey" },
-    @{ name = "MicaForEveryone.MicaForEveryone" },
-    @{ name = "Microsoft.dotnetRuntime.6-x64" },
-    @{ name = "Microsoft.PowerShell" },
-    @{ name = "Microsoft.PowerToys" },
-    @{ name = "Microsoft.Sysinternals.Autoruns"; admin = "true" },
-    @{ name = "Microsoft.Sysinternals.ProcessExplorer"; admin = "true" },
-    @{ name = "Microsoft.Sysinternals.ProcessMonitor"; admin = "true" },
-    @{ name = "Microsoft.Teams" },
-    @{ name = "Microsoft.VisualStudioCode" },
-    @{ name = "Microsoft.WindowsTerminal" },
-    @{ name = "MikeFarah.yq" },
-    @{ name = "Notion.Notion" },
-    @{ name = "OBSProject.OBSStudio" },
-    @{ name = "QL-Win.QuickLook" },
-    @{ name = "SlackTechnologies.Slack" },
-    @{ name = "Spotify.Spotify"; force = "true" },
-    @{ name = "StartIsBack.StartAllBack" },
-    @{ name = "stedolan.jq" },
-    @{ name = "stnkl.EverythingToolbar" },
-    @{ name = "SyncTrayzor.SyncTrayzor" },
-    @{ name = "Tailscale" },
-    # @{ name = "TranslucentTB" },
-    @{ name = "Valve.Steam" },
-    @{ name = "voidtools.Everything" },
-    @{ name = "WinDirStat.WinDirStat" },
+    @{ name = "AgileBits.1Password.CLI" }
+    @{ name = "AltSnap.AltSnap" }
+    @{ name = "Audacity.Audacity" }
+    @{ name = "Discord.Discord" }
+    @{ name = "Ferdium.Ferdium" }
+    @{ name = "File-New-Project.EarTrumpet" }
+#    @{ name = "Flameshot.Flameshot" }
+    @{ name = "gerardog.gsudo" }
+    @{ name = "Giorgiotani.Peazip" }
+    @{ name = "Git.Git"; interactive = "true" }
+    @{ name = "GitHub.GitHubDesktop" }
+    @{ name = "GnuPG.Gpg4win"; interactive = "true" }
+    @{ name = "Google.Chrome"; force = "true" }
+    @{ name = "gsass1.NTop" }
+    @{ name = "Hashicorp.Packer" }
+    @{ name = "Hashicorp.Terraform" }
+    @{ name = "JackieLiu.NotepadsApp" }
+    @{ name = "JanDeDobbeleer.OhMyPosh" }
+    @{ name = "Keybase" }
+    @{ name = "Lexikos.AutoHotkey" }
+    @{ name = "MicaForEveryone.MicaForEveryone" }
+    @{ name = "Microsoft.dotnetRuntime.6-x64" }
+    @{ name = "Microsoft.PowerShell" }
+    @{ name = "Microsoft.PowerToys" }
+    @{ name = "Microsoft.Sysinternals.Autoruns"; admin = "true" }
+    @{ name = "Microsoft.Sysinternals.ProcessExplorer"; admin = "true" }
+    @{ name = "Microsoft.Sysinternals.ProcessMonitor"; admin = "true" }
+    @{ name = "Microsoft.Teams" }
+    @{ name = "Microsoft.VisualStudioCode" }
+    @{ name = "Microsoft.WindowsTerminal" }
+    @{ name = "MikeFarah.yq" }
+    # @{ name = "Notion.Notion" }
+    @{ name = "OBSProject.OBSStudio" }
+    @{ name = "QL-Win.QuickLook" }
+    # @{ name = "SlackTechnologies.Slack" }
+    @{ name = "SomePythonThings.WingetUIStore" }
+    @{ name = "Spotify.Spotify"; force = "true" }
+    @{ name = "StartIsBack.StartAllBack" }
+    @{ name = "stedolan.jq" }
+    @{ name = "stnkl.EverythingToolbar" }
+    @{ name = "SyncTrayzor.SyncTrayzor" }
+    @{ name = "Tailscale" }
+    # @{ name = "TranslucentTB" }
+    @{ name = "Valve.Steam" }
+    @{ name = "voidtools.Everything" }
+    @{ name = "WinDirStat.WinDirStat" }
     @{ name = "Zoom.Zoom" }
-    # @{ name = "AltSnap.AltSnap" },
-    # @{ name = "EpicGames.EpicGamesLauncher" },
-    # @{ name = "GIMP.GIMP" },
-    # @{ name = "lars-berger.GlazeWM" }
-    # @{ name = "LGUG2Z.komorebi" },
-    # @{ name = "Logitech.Options" },
-    # @{ name = "Microsoft.NuGet"; admin = "true" },
-    # @{ name = "MoonlightGameStreamingProject.Moonlight" },
-    # @{ name = "Notion.Notion" },
-    # @{ name = "Nvidia.GeForceExperience" },
-    # @{ name = "Playnite.Playnite" },
-    # @{ name = "rickbutton.workspacer.beta"; force = "true" },
-    # @{ name = "SomePythonThings.WingetUIStore" },
 )
 
 #-------------------------------------------------------------------------------
